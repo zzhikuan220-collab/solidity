a0476fefa30c58bfbf56631b41d3286b08c5f265
clonker <1685266+clonker@users.noreply.github.com>
2025-04-01 09:07:27 +0200

    add value description

--- libyul/backends/evm/SSAControlFlowGraph.cpp
+++ libyul/backends/evm/SSAControlFlowGraph.cpp
@@ -53,7 +53,7 @@ public:
 		stream << printer.m_result.str();
 		return stream;
 	}
-private:
+
 	static std::string varToString(SSACFG const& _cfg, SSACFG::ValueId _var) {
 		if (_var.value == std::numeric_limits<size_t>::max())
 			return "INVALID";
@@ -77,6 +77,7 @@ private:
 		);
 	}
 
+private:
 	static std::string escape(std::string_view const str)
 	{
 		using namespace std::literals;
@@ -312,4 +313,9 @@ std::string SSACFG::toDot(
 	if (_includeDiGraphDefinition)
 		output << "}\n";
 	return output.str();
+}
+
+std::string SSACFG::valueDescription(ValueId const& _valueId) const
+{
+	return SSACFGPrinter::varToString(*this, _valueId);
 }
--- libyul/backends/evm/SSAControlFlowGraph.h
+++ libyul/backends/evm/SSAControlFlowGraph.h
@@ -226,6 +226,8 @@ public:
 		return *idx;
 	}
 
+	std::string valueDescription(ValueId const& _valueId) const;
+
 	std::string toDot(
 		bool _includeDiGraphDefinition=true,
 		std::optional<size_t> _functionIndex=std::nullopt,
