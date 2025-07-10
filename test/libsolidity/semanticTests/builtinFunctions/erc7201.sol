ea67ebd50bcd629df413c788b5e81ab7ab2fa2e6
Matheus Aguiar <matheus.pit@gmail.com>
2025-07-10 12:10:08 -0300

    fixup! Add and update tests

--- test/libsolidity/semanticTests/builtinFunctions/erc7201.sol
+++ test/libsolidity/semanticTests/builtinFunctions/erc7201.sol
@@ -5,7 +5,11 @@ contract C {
     function g() public pure returns (bool) {
         return erc7201("1234") == erc7201("1234");
     }
+    function h() public pure returns (bool) {
+        return erc7201("ABCD") != erc7201("1234");
+    }
 }
 // ----
 // f() -> -37027169335357044721174777765900489039992824610707613329797121153713850199808
 // g() -> true
+// h() -> true
